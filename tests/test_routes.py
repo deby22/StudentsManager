9480fbfb4550349ddd14e935a8712b5986e34ba3
Dawid Dęby <dawid.deby@gmail.com>
2021-12-08 08:50:21 +0100

    add missing test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -77,3 +77,62 @@ def test_delete_student_with_existing_student_should_return_entity(client):
 
     # Assert
     assert response.status_code == 404
+
+
+def test_update_student_with_existing_student_and_empty_json_data_should_not_update_entity(
+    client,
+):
+    # Arrange
+    student = factory.build(dict, FACTORY_CLASS=StudentFactory)
+    created_student = client.post(
+        "/students", data=json.dumps(student), content_type="application/json"
+    ).json["id"]
+
+    # Act
+    response = client.put(
+        f"/students/{created_student}",
+        data=json.dumps({}),
+        content_type="application/json",
+    )
+
+    # Assert
+    assert response.status_code == 200
+    assert response.json["name"] == student["name"]
+    assert response.json["surname"] == student["surname"]
+    assert response.json["specialization"] == student["specialization"]
+
+
+def test_update_student_with_existing_student_and_non_empty_json_data_should_not_update_entity(
+    client,
+):
+    # Arrange
+    student = factory.build(dict, FACTORY_CLASS=StudentFactory)
+    created_student = client.post(
+        "/students", data=json.dumps(student), content_type="application/json"
+    ).json["id"]
+    updated_student = {
+        "name": "New name",
+        "surname": "New surname",
+        "specialization": "New specialization",
+    }
+
+    # Act
+    response = client.put(
+        f"/students/{created_student}",
+        data=json.dumps(updated_student),
+        content_type="application/json",
+    )
+
+    # Assert
+    assert response.status_code == 200
+    assert response.json["name"] == updated_student["name"]
+    assert response.json["surname"] == updated_student["surname"]
+    assert response.json["specialization"] == updated_student["specialization"]
+
+
+def test_update_student_with_non_existing_student_should_not_update_entity(client):
+    # Act
+    response = client.get(f"/students/{uuid.uuid4()}")
+
+    # Assert
+    assert response.status_code == 404
